77ffd98be82e2ff1312eb9d25013c80fd05dc842
Ana Barros <ana.rodriguesb@hotmail.com>
2022-11-23 19:03:06 -0300

    Finish

--- src/Jogo.java
+++ src/Jogo.java
@@ -1,11 +1,66 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public record Jogo(Player user, Player IA, int rounds) {
     public void toPlay(){
         System.out.println("\n******* SEJA BEM-VINDO(A), " + user.getName() + " *******\n");
+
+        for(int i = 1; i <= rounds; i++){
         int choiseUser = choiseUser();
+        if(choiseUser < 1 || choiseUser > 3) {
+            System.out.println("\nJOGADA INVALIDA: (1, 2 ou 3) :");
+            System.out.println("\tPONTO PARA  " + IA.getName() + "\n");
+            IA.incrementScore();
+            //continue;
+        }
+
+            int choiseIA = choiseIA();
+            System.out.print("\n" + choiseUser + " X " + choiseIA + "\n");
+
+            int result = choiseUser - choiseIA;
+
+            winnerRound(result);
+        }
     }
-    private int choiseUser (){
+
+    public void showFinalResult () {
+        System.out.println("\n****************************************\n");
+        Integer finalScoreUser = user.getScore();
+        Integer finalScoreIA = IA.getScore();
+
+        System.out.println("\n\tPLACAR FINAL: " + user.getName() + " " + user.getScore() + " X " + IA.getScore() + " " + IA.getName());
+
+        if(finalScoreUser == finalScoreIA){
+            System.out.println("EMPATE!");
+        } else {
+            String finalWinner = (finalScoreIA > finalScoreUser) ? IA.getName() : user.getName();
+            System.out.println("VENCEDOR = " + finalWinner.toUpperCase());
+        }
+        System.out.println("\n****************************************\n");
+    }
+    private void winnerRound (int result) {
+        String winnerRound;
+        if (result  == 0){
+            winnerRound = "EMPATE";
+        } else {
+            if(result ==-1 || result == 2) {
+                IA().incrementScore();
+                winnerRound = IA().getName();
+            } else {
+                user.incrementScore();
+                winnerRound = user.getName();
+            }
+        }
+        System.out.println("\n Vencedor do Round:" + winnerRound + "\n");
+    }
+
+    private int choiseIA() {
+        Random random = new Random();
+        return random.nextInt(3 ) + 1;
+
+    }
+
+    private int choiseUser(){
      Scanner scan = new Scanner(System.in);
      System.out.println("1 - PEDRA");
      System.out.println("2 - PAPEL");
